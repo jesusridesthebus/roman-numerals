88289f261e92ae07b776e319f73602266cd62ccb
Liz Kelley and Reese Lee <reese@>
2019-04-04 15:45:37 -0700

    merge functions, move global variables to local variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 var arabicNumbersList = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 var romanNumeralList = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 
-function convert(number) {
-  if (input > 3999) {
-    return "Roman Numerals do not go higher than 3,999";
-  } else {
-    console.log(input);
-  }
-};
+// function convert(number) {
+//   if (input > 3999) {
+//     return "Roman Numerals do not go higher than 3,999";
+//   } else {
+//     console.log(input);
+//   }
+// };
 
-var romanNumeral = "";
-function returnNumeral (number){
-  for (var i = arabicNumbersList.length-1; i >= 0; i--) {
+function returnNumeral (number) {
+  var romanNumeral = "";
+  if (number > 3999) {
+    return "Roman Numerals do not go higher than 3,999";
+  } for (var i = arabicNumbersList.length-1; i >= 0; i--) {
     while (number >= arabicNumbersList[i]) {
       number -= arabicNumbersList[i];
       romanNumeral += romanNumeralList[i];
@@ -25,7 +27,7 @@ $(function() {
     $("#result").hide();
     var numberInput = parseInt($("#integer-input").val());
     var result = returnNumeral(numberInput);
-    $("#result").show(result);
     $(".conversion").text(result);
+    $("#result").show(result);
   });
 });
