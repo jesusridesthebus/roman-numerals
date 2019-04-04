7037fb56bad89d425d75249369930d04aa17a91e
Liz Kelley and Reese Lee <liz@>
2019-04-04 15:47:05 -0700

    clean up code and add comment to clarify moving global var to local var

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
 var arabicNumbersList = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 var romanNumeralList = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 
-// function convert(number) {
-//   if (input > 3999) {
-//     return "Roman Numerals do not go higher than 3,999";
-//   } else {
-//     console.log(input);
-//   }
-// };
-
 function returnNumeral (number) {
-  var romanNumeral = "";
+  var romanNumeral = ""; //make it a local var to refresh answers
   if (number > 3999) {
     return "Roman Numerals do not go higher than 3,999";
   } for (var i = arabicNumbersList.length-1; i >= 0; i--) {
