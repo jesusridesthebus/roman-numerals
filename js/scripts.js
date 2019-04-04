7b3f02679a7b3157b2432130aa0c36a8b5022704
Liz Kelley and Reese Lee <liz@>
2019-04-04 14:53:48 -0700

    add roman numerals array, add function to access arrays and return the correct converted roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
-var arabicNumbersList = ["1", "4", "5", "9", "10", "40", "50", "90", "100", "400", "500", "900", "1000"];
-var romanNumeralList = [];
+var arabicNumbersList = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+var romanNumeralList = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 
-function romanNumeral(input) {
+function convert(number) {
   if (input > 3999) {
     return "Roman Numerals do not go higher than 3,999";
-  } else if
+  } else {
+    console.log(input);
+  }
 };
 
-
+var romanNumeral = "";
+function returnNumeral (number){
+  for (var i = arabicNumbersList.length-1; i >= 0; i--) {
+    while (number >= arabicNumbersList[i]) {
+      number -= arabicNumbersList[i];
+      romanNumeral += romanNumeralList[i];
+    }
+  } return romanNumeral;
+};
 
 $(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
+    $("#result").hide();
     var numberInput = parseInt($("#integer-input").val());
-    var result = romanNumeral(numberInput);
+    var result = returnNumeral(numberInput);
     $("#result").show(result);
     $(".conversion").text(result);
   });
